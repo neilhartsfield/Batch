054d3d0706b06b673ddc639d059aa4184dd288d7
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2021-02-27 22:54:44 -0600

    Create SetLogging.bat

---
+++ SetLogging.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM Method 1 (Pretty logging)
+If Exist "%SYSTEMROOT%\temp\mcafeelogs\" (set BATLOG="%SYSTEMROOT%\Temp\my\log.log")
+	else (
+	mkdir "%SYSTEMROOT%\Temp\my\"
+	set BATLOG="%SYSTEMROOT%\Temp\my\log.log"
+)
+
+echo ************************************** >> %BATLOG%
+echo       %date% %time%               		>> %BATLOG%
+echo ************************************** >> %BATLOG%
+
+:: Rest of script here
+
+
+REM Method 2 (Short and sweet)
+
+@echo off
+set BATLOG=%SYSTEMROOT%\Temp\bat.log
+call :LOG >> %BATLOG%
+exit /B
+
+:LOG
+
+:: Rest of script here
