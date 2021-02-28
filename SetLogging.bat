d578abcefb606c394f180fcdef559837f5b9f30b
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2021-02-28 12:02:02 -0600

    Update SetLogging.bat

--- SetLogging.bat
+++ SetLogging.bat
@@ -1,13 +1,31 @@
-
-
-REM Method 1 (Pretty logging):
+REM Method 1 (Short and sweet):
 
 @echo off
-If Exist "%SYSTEMROOT%\temp\mcafeelogs\" (set BATLOG="%SYSTEMROOT%\Temp\my\log.log")
-	else (
-	mkdir "%SYSTEMROOT%\Temp\my\"
-	set BATLOG="%SYSTEMROOT%\Temp\my\log.log"
-)
+setlocal ENABLEDELAYEDEXPANSION
+
+set BATLOG=%SYSTEMROOT%\Temp\bat.log
+call :LOG >> %BATLOG%
+exit /B
+
+:LOG
+
+echo ************************************** 
+echo       %date% %time%               		
+echo ************************************** 
+
+:: Rest of script here
+
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+REM Method 2 (Pretty logging):
+
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+if exist "%SYSTEMROOT%\Temp\my\" (SET BATLOG="%SYSTEMROOT%\Temp\my\bat.log") else (mkdir "%SYSTEMROOT%\Temp\my\")
+set BATLOG="%SYSTEMROOT%\Temp\my\bat.log"
 
 echo ************************************** >> %BATLOG%
 echo       %date% %time%               		>> %BATLOG%
@@ -18,13 +36,21 @@ echo ************************************** >> %BATLOG%
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-REM Method 2 (Short and sweet):
+REM Method 3 (take user input)
 
 @echo off
-set BATLOG=%SYSTEMROOT%\Temp\bat.log
-call :LOG >> %BATLOG%
-exit /B
+setlocal ENABLEDELAYEDEXPANSION
 
-:LOG
+REM Set the batlog up & ask for target log directory location
+set /p inputpath="Set log directory (full path): "
+
+REM make the directory path if it doesn't exist & set the batlog location
+if exist "%INPUTPATH%" (SET BATLOG="%INPUTPATH%\bat.log") else (mkdir "%INPUTPATH%")
+set BATLOG="%INPUTPATH%\bat.log"
+
+REM populate batlog title
+echo ************************************** >> %BATLOG%
+echo       %date% %time%               		>> %BATLOG%
+echo ************************************** >> %BATLOG%
 
 :: Rest of script here
